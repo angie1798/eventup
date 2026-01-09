e99dca8d8f258ca493fb2b3246630b1c1cbeb8f4
angie1798 <angie179836@gmail.com>
2026-01-08 19:34:23 -0600

    tabla y subir excel

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,117 @@
-import Image from "next/image";
+"use client"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
+import { useRef, useState } from "react";
+import * as XLSX from "xlsx";
 
+export interface PersonaExcel {
+  id: number;
+  nombre: string;
+  apellido: string;
+}
 export default function Home() {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [data, setData] = useState<PersonaExcel[]>([]);
+  const [fileName, setFileName] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+   let personas = [
+  { "id": 1, "nombre": "Ana", "apellido": "Rodríguez" },
+  { "id": 2, "nombre": "Carlos", "apellido": "Méndez" },
+  { "id": 3, "nombre": "María", "apellido": "Gómez" },
+  { "id": 4, "nombre": "José", "apellido": "Hernández" },
+  { "id": 5, "nombre": "Laura", "apellido": "Vargas" },
+  { "id": 6, "nombre": "Daniel", "apellido": "Castro" },
+  { "id": 7, "nombre": "Sofía", "apellido": "Ramírez" },
+  { "id": 8, "nombre": "Andrés", "apellido": "Morales" },
+  { "id": 9, "nombre": "Valeria", "apellido": "Jiménez" },
+  { "id": 10, "nombre": "Diego", "apellido": "Rojas" },
+  { "id": 11, "nombre": "Paula", "apellido": "Navarro" },
+  { "id": 12, "nombre": "Luis", "apellido": "Alvarado" },
+  { "id": 13, "nombre": "Camila", "apellido": "Salas" },
+  { "id": 14, "nombre": "Javier", "apellido": "Pérez" },
+  { "id": 15, "nombre": "Natalia", "apellido": "Cordero" },
+  { "id": 16, "nombre": "Fernando", "apellido": "Araya" },
+  { "id": 17, "nombre": "Lucía", "apellido": "Soto" },
+  { "id": 18, "nombre": "Ricardo", "apellido": "Chaves" },
+  { "id": 19, "nombre": "Elena", "apellido": "Mora" },
+  { "id": 20, "nombre": "Miguel", "apellido": "Pacheco" }
+]
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    setIsLoading(true);
+    setFileName(file.name);
+
+    try {
+      const buffer = await file.arrayBuffer();
+      const workbook = XLSX.read(buffer, { type: "array" });
+
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      const jsonData = XLSX.utils.sheet_to_json<PersonaExcel>(sheet, {
+        defval: null,
+      });
+
+      setData(jsonData);
+      personas = data
+      console.log("Datos Excel:", jsonData);
+    } catch (error) {
+      console.error("Error leyendo el Excel", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="container  mx-auto mt-20">
+      <Table>
+  <TableCaption>Tu lista de invitados.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Número</TableHead>
+      <TableHead>Nombre</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {data.length > 0 ?
+    data.map((persona) => (
+      <TableRow key={persona.id}>
+        <TableCell className="font-medium">INV{persona.id.toString().padStart(3, '0')}</TableCell>
+        <TableCell>{persona.nombre} {persona.apellido}</TableCell>
+      </TableRow>
+    )): (<TableRow><TableCell colSpan={2}>Actualmente no tienes invitados</TableCell></TableRow>)}
+
+  </TableBody>
+</Table>
+
+<Button className="mt-4 mb-20" onClick={handleButtonClick}>Subir lista de invitados</Button>
+
+      <input
+        type="file"
+        ref={fileInputRef}
+        accept=".xlsx,.xls"
+        className="hidden"
+        onChange={handleFileChange}
+      />
     </div>
   );
 }
